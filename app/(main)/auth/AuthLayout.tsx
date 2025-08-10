30dc201c3eee044e0217e33b1ccd4fe5d68dc1f1
favourachara07 <acharafavour01@gmail.com>
2025-08-10 18:35:23 +0100

    Fixed type errors

--- app/(main)/auth/AuthLayout.tsx
+++ app/(main)/auth/AuthLayout.tsx
@@ -1,20 +1,20 @@
-'use client'
+"use client";
 
-import Chatbot from "@/app/components/common/Chatbot";
-import Footer from "@/app/components/common/Footer";
-import Navbar from "@/app/components/common/Navbar";
-import { usePathname } from "next/navigation";
+// import Chatbot from "@/app/components/common/Chatbot";
+// import Footer from "@/app/components/common/Footer";
+// import Navbar from "@/app/components/common/Navbar";
+// import { usePathname } from "next/navigation";
 
-export function AuthLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isAuthPage = pathname.startsWith("/auth");
+// export function AuthLayout({ children }: { children: React.ReactNode }) {
+//   const pathname = usePathname();
+//   const isAuthPage = pathname.startsWith("/auth");
 
-  return (
-    <>
-      {!isAuthPage && <Navbar />}
-      {children}
-      {!isAuthPage && <Footer />}
-      {!isAuthPage && <Chatbot />}
-    </>
-  );
-}
+//   return (
+//     <>
+//       {!isAuthPage && <Navbar />}
+//       {children}
+//       {!isAuthPage && <Footer />}
+//       {!isAuthPage && <Chatbot />}
+//     </>
+//   );
+// }
